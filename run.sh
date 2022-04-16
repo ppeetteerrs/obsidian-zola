bd485ccd077b2c515533f9af60f6da895a417e3c
Peter Yuen <ppeetteerrs@Peter.localdomain>
2022-04-17 06:46:42 +0800

    Updated remote run script

--- run.sh
+++ run.sh
@@ -3,15 +3,19 @@
 pip install python-slugify
 
 # Avoid copying over netlify.toml (will ebe exposed to public API)
-echo "netlify.toml" >> __obsidian/.gitignore
+echo "netlify.toml" >>__obsidian/.gitignore
 
 # Sync Zola template contents
 rsync -a __site/zola/ __site/build
 rsync -a __site/content/ __site/build/content
 
 # Use obsidian-export to export markdown content from obsidian
-mkdir -p __site/build/content/docs __site/build/__docs
-__site/bin/obsidian-export --frontmatter=never --hard-linebreaks --no-recursive-embeds __obsidian __site/build/__docs
+mkdir -p build/content/docs build/__docs
+if [ -z "$STRICT_LINE_BREAKS" ]; then
+	bin/obsidian-export --frontmatter=never --hard-linebreaks --no-recursive-embeds __obsidian __site/build/__docs
+else
+	bin/obsidian-export --frontmatter=never --no-recursive-embeds __obsidian __site/build/__docs
+fi
 
 # Run conversion script
 python __site/convert.py
